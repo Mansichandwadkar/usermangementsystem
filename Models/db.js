f917ec317f7d9814fd7014361c11ceb851c6bfe4
Mansichandwadkar <90907366+Mansichandwadkar@users.noreply.github.com>
2021-09-17 17:40:34 +0530

    Add files via upload

--- Models/db.js
+++ Models/db.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
 
+const url = "mongodb://localhost:27017/EmployeeDB";
+
+mongoose.connect(url,{useNewUrlParser:true},(err) => {
+    if(!err){ console.log("MongoDB Connection Succeeded");}
+    else{
+        console.log("An Error Occured in Connecting mongodb" + err);
+    } 
+})
+
+
+require('./employee.model');
